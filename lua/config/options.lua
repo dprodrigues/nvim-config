06bdcb037f1e266960f74b264494fa269bea22c9
Daniel Rodrigues <danielprodrigues0@gmail.com>
2025-10-12 20:17:40 -0300

    initial commit

---
+++ lua/config/options.lua
@@ -0,0 +1,18 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.number = true -- show numbers
+vim.opt.relativenumber = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.clipboard = "unnamedplus"
